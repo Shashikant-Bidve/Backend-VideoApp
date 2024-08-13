9aa8943a9803af7675ff8a499b43ac33a1924aba
Shashikant Bidve <shashi.bidve18@gmail.com>
2024-08-13 14:40:08 +0530

    watch history endpoint added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -391,6 +391,55 @@ const getUserChannelProfile = asyncHandler( async(req, res) => {
 
 });
 
+const getWatchHistory = asyncHandler( async(req, res) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },{
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullname: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+
+                            
+                        }
+                    },
+                    {
+                        $addFields:{
+                            owner:{
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res.status(200).json(
+        new ApiResponse(200, user[0].watchHistory, "watch history fetched successfully.")
+    )
+});
+
 
 
 
